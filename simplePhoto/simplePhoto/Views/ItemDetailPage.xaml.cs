cf6c7829905869b29854b7bce3919965cf3c7a11
yourespeakingtothemoon <manen@tutanota.com>
2022-11-09 09:33:53 -0700

    Add project files.

---
+++ simplePhoto/simplePhoto/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using simplePhoto.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace simplePhoto.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
