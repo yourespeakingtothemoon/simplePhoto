a3c55b0b3dec3c09ffc31e9f97c2afb3e1b2101b
EthanBarlevy <98473724+EthanBarlevy@users.noreply.github.com>
2022-11-11 08:58:35 -0700

    Added a color class because SKColor is dumb

--- simplePhoto/simplePhoto/Views/AboutPage.xaml.cs
+++ simplePhoto/simplePhoto/Views/AboutPage.xaml.cs
@@ -97,4 +97,82 @@ namespace simplePhoto.Views
             }
         }  
     }
+
+    public class Color
+    {
+        public int r, g, b, a;
+
+        public Color(int r, int g, int b)
+        {
+            this.r = r;
+            this.g = g;
+            this.b = b;
+            this.a = 255;
+        }
+
+        public Color(int r, int g, int b, int a)
+        {
+            this.r = r;
+            this.g = g;
+            this.b = b;
+            this.a = a;
+        }
+
+        /// <summary>
+        /// Convert 
+        /// </summary>
+        /// <param name="color"></param>
+        /// <returns></returns>
+        public static Color SKColorToColor(SKColor color)
+        {
+            return new Color(color.Red, color.Green, color.Blue, color.Alpha);
+        }
+
+        /// <summary>
+        /// Convert Color to an SKColor
+        /// </summary>
+        /// <param name="color"></param>
+        /// <returns></returns>
+        public static SKColor ColorToSKColor(Color color)
+        {
+            return new SKColor((byte)color.r, (byte)color.g, (byte)color.b, (byte)color.a);
+        }
+
+        /// <summary>
+        /// Takes a color reference and makes all of the values between 255 and 0
+        /// </summary>
+        /// <param name="color"></param>
+        public static void toByte(ref Color color)
+        {
+            color.r = (color.r > 255) ? 255 : color.r ;
+            color.r = (color.r < 0) ? 0 : color.r ;
+            color.g = (color.g > 255) ? 255 : color.g ;
+            color.g = (color.g < 0) ? 0 : color.g ;
+            color.b = (color.b > 255) ? 255 : color.b;
+            color.b = (color.b < 0) ? 0 : color.b;
+            color.a = (color.a > 255) ? 255 : color.a;
+            color.a = (color.a < 0) ? 0 : color.a;
+        }
+
+        public static void Add (ref int color, int i)
+        {
+            color += i;
+        }
+
+        public static void Add(ref Color color, ref Color color2)
+        {
+            color.r += color2.r;
+            color.g += color2.g;
+            color.b += color2.b;
+            color.a += color2.a;
+        }
+
+        public static void SetColor(ref Color color, int r, int g, int b, int a)
+        {
+            color.r = r;
+            color.g = g;
+            color.b = b;
+            color.a = a;
+        }
+    }
 }
