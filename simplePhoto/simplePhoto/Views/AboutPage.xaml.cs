c1817145bbafed2cf8dca983c58fd7f1d5484b74
yourespeakingtothemoon <manen@tutanota.com>
2022-11-10 17:55:59 -0700

    Randomized Color Filter, Basic pixel color replacement loop is implemented

--- simplePhoto/simplePhoto/Views/AboutPage.xaml.cs
+++ simplePhoto/simplePhoto/Views/AboutPage.xaml.cs
@@ -43,15 +43,47 @@ namespace simplePhoto.Views
                         MainImage.Source = ImageSource.FromStream(() => stream);
                         SKBitmap skb = new SKBitmap();
                         skb = SKBitmap.Decode(stream2);
-                        byte[] data = skb.Bytes;
+                        /*byte[] data = skb.Bytes;
                         string str = "";
                         foreach (var item in data)
                         {
                             str += item + ", ";
                         }
                         Label label = new Label { Text = str };
-                        layout.Children.Add(label);
+                        layout.Children.Add(label);*/
                         FileName.Text = Text;
+                        Random random = new Random();
+                        for (int x = 0; x < skb.Width; x++)
+                        {
+                            for (int y = 0; y < skb.Height; y++)
+                            {
+                                int hueAdd = random.Next(0, 50);
+                                int satAdd = random.Next(0, 50);
+                                int valAdd = random.Next(0, 50);
+
+                                SKColor pixelColor = skb.GetPixel(x, y);
+                                SKColor newColor = new SKColor();
+                                newColor = SKColor.FromHsv(pixelColor.Hue+hueAdd, satAdd,pixelColor.Alpha+valAdd);
+                                skb.SetPixel(x, y, newColor);
+                                //Console.WriteLine("Pixel Done!");
+
+                            }
+                        }
+                        SKData skd = skb.Encode(SKEncodedImageFormat.Png,100);
+                        Stream stream3 = new MemoryStream();
+                        stream3 = skd.AsStream();
+                        MainImage.Source = ImageSource.FromStream(() => stream3);
+                        /*byte[] data2 = skb.Bytes;
+                        string str2 = "";
+                        foreach (var item in data2)
+                        {
+                            str2 += item + ", ";
+                        }
+                        Label label2 = new Label { Text = str2 };
+                        layout.Children.Add(label2);*/
+
+                        //MainImage.ScaleTo(100.5);
+
                     }
                     else
                     {
