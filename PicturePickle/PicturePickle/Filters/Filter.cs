e1e19c0d86335425ed122f456793bcd0188d4bcb
yourespeakingtothemoon <manen@tutanota.com>
2022-11-21 09:09:30 -0700

    added filter abstract class, added declaration to ISave

---
+++ PicturePickle/PicturePickle/Filters/Filter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PicturePickle
+{
+    // base filter class that all other filters will inherit from
+    public abstract class Filter
+    {
+        public void addLayer(Color color,int behavior);
+        public void removeLayer(Color color,int behavior);
+
+        public Color[] colors;
+
+        public int[] behaviors;
+
+        public Color execute(Color pixColor);
+    }
+}
